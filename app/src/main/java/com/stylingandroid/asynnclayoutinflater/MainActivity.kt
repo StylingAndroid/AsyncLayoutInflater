c2ad2bbc72dfac2959a5c11136fd371626407795
Mark Allison <mark@stylingandroid.com>
2020-10-30 16:37:57 +0000

    Initial commit

---
+++ app/src/main/java/com/stylingandroid/asynnclayoutinflater/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.stylingandroid.asynnclayoutinflater
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stylingandroid.asynnclayoutinflater.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
